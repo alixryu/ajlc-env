bea3d02459b478bd0842d0a4f92e3057de408c2b
heavenk21 <heavenk21@gmail.com>
2016-01-04 16:03:59 -0500

    Fix to-be-symbolic-linked file extention to .link

---
+++ pondfile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DOTHOME=$HOME/ajlc-env
+linkables=$( find -H "$DOTHOME" -maxdepth 2 -name "*.link" )
+
+echo -e "\nCreating symbolic links..."
+echo "=============================="
+for source in $linkables ; do
+    target="$HOME/.$( basename $source ".link" )"
+    if [ -e $target ]; then
+        echo "~${target#$HOME} already exists. Skipping to next file"
+    else
+        ln -s $source $target
+        echo "Created symbolic link for $source"
+    fi
+done
+echo "Symbolic link creation complete"
